0a2c22af8443076a923642ee4b923c225747110f
Seba DF <spinto@df.uba.ar>
2026-03-31 16:08:55 -0300

    Minimal frontend added

--- src/app.py
+++ src/app.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
 from src.schemas import AnalysisInput, AnalysisOutput
 from src.service import run_analysis
 
@@ -7,3 +10,9 @@ app = FastAPI()
 @app.post("/analyze", response_model=AnalysisOutput)
 def analyze(input_data: AnalysisInput):
     return run_analysis(input_data.data)
+
+@app.get("/")
+def read_index():
+    return FileResponse("src/static/index.html")
+
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
